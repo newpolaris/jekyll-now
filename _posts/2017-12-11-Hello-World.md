e0d11a478b1abe53610d856c161f321b85d7177a
newpolaris <newpolaris@gmail.com>
2017-12-12 20:24:27 +0900

    use path

--- _posts/2017-12-11-Hello-World.md
+++ _posts/2017-12-11-Hello-World.md
@@ -14,8 +14,22 @@ int main()
   std::cout << "Hello world\n" << std::endl;
   return 0;
 }
+<script src="/code/lib/demo/helloworld.js"></script>
+<script>helloworld_demo('helloworld-demo')</script>
 ```
 <div id='helloworld-demo'></div>
 <script src="//cdnjs.cloudflare.com/ajax/libs/three.js/88/three.min.js"></script>
-<script src="/code/lib/demo/helloworld.js"></script>
-<script>helloworld_demo('helloworld-demo')</script>
+<script src='/lib/require.js'></script>
+<script>
+	requirejs.config({
+	baseUrl: '/',
+	paths: {
+		require: 'lib/require',
+		math:    'lib/math.min',
+		code:    'code/lib'
+	}
+	});
+</script>
+<script>
+	require(["code/demo/helloworld"], function (init) { init('helloworld-demo'); });
+</script>
--- code/lib/demo/helloworld.js
+++ code/lib/demo/helloworld.js
@@ -1,35 +1,43 @@
-function helloworld_demo(element) {
-	var div = document.getElementById(element);
-	div.style.display       = 'flex';
-	div.style.flexDirection = 'column';
-	div.style.flexWrap      = 'nowrap';
-	div.style.alignItems    = 'stretch';
+define ([
+	'math',
+	'code/util/demo-init'
+], function(
+	math,
+	init
+) {
+	return function(id) {
+		var div = document.getElementById(id);
+		div.style.display       = 'flex';
+		div.style.flexDirection = 'column';
+		div.style.flexWrap      = 'nowrap';
+		div.style.alignItems    = 'stretch';
 
-	var w = 640;
-	var h = w * 9 / 16;
-	var canvas = document.createElement('canvas');
-	canvas.width  = w;
-	canvas.height = h;
-	canvas.style.margin    = '1em';
-	canvas.style.alignSelf = 'center';
+		var w = 640;
+		var h = w * 9 / 16;
+		var canvas = document.createElement('canvas');
+		canvas.width  = w;
+		canvas.height = h;
+		canvas.style.margin    = '1em';
+		canvas.style.alignSelf = 'center';
 
-	var scene = new THREE.Scene();
-	var aspect = window.innerWidth / window.innerHeight;
-	var camera = new THREE.PerspectiveCamera( 75, aspect, 0.1, 1000 );
-	var renderer = new THREE.WebGLRenderer( { canvas: canvas } );
-	div.appendChild(canvas);
+		var scene = new THREE.Scene();
+		var aspect = window.innerWidth / window.innerHeight;
+		var camera = new THREE.PerspectiveCamera( 75, aspect, 0.1, 1000 );
+		var renderer = new THREE.WebGLRenderer( { canvas: canvas } );
+		div.appendChild(canvas);
 
-	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-	var material = new THREE.MeshNormalMaterial();
-	var cube = new THREE.Mesh( geometry, material );
-	scene.add( cube );
-	camera.position.z = 5;
+		var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+		var material = new THREE.MeshNormalMaterial();
+		var cube = new THREE.Mesh( geometry, material );
+		scene.add( cube );
+		camera.position.z = 5;
 
-	var render = function() {
-		requestAnimationFrame( render );
-		cube.rotation.x += 0.1;
-		cube.rotation.y += 0.1;
-		renderer.render( scene, camera );
-	};
-	render();
-};
+		var render = function() {
+			requestAnimationFrame( render );
+			cube.rotation.x += 0.1;
+			cube.rotation.y += 0.1;
+			renderer.render( scene, camera );
+		};
+		render();
+	}
+});
